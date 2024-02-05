8f39721533e5008a9af2209d0d9a5ba4bddc6fa0
Chan Dinh <dinhhoangbaochan@gmail.com>
2024-02-05 20:31:36 +0700

    create plugin entry info

---
+++ viecommerce.php
@@ -0,0 +1,14 @@
+<?php
+/**
+ * Plugin Name: VieCommerce
+ * Plugin URI: https://chandinh.com
+ * Description: An ecommerce plugin from Vietnamese dev community. Just want to try and be a competitor with the great Woo.
+ * Version: 0.0.1
+ * Author: Chan Dinh
+ * Author URI: https://chandinh.com
+ * Text Domain: viecommerce
+ * Requires at least: 6.3
+ * Requires PHP: 7.4
+ *
+ * @package VieCommerce
+ */
